001f1eb45b125a45cc35682f2af73391c9af2c8f
moahmed amine mezghich <git@mezghich.com>
2023-03-13 16:13:08 +0100

    structure angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ams';
+  logo='https://i.twic.pics/v1/contain=490x280/https://s3-eu-west-1.amazonaws.com/assets.atout-on-line.com/logos/formation/122271565658ff1952bcf76.jpg'
+  formation = 'Angular';
+
+  candidats:string[]=["olivier","nicolas","alex","amine"];
+
+  display()
+  {
+      alert("Hello from Angular");
+  }
 }
